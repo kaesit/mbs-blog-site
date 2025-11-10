84cc7782f160b488485474aac41a57f54f59d466
esadabdullahkosedag <kaesoftware@gmail.com>
2025-11-10 21:26:33 +0300

    about page has revisited

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,6 +6,10 @@ import ChromaGrid from "../components/ChromaGrid";
 import ai_icon from "../icons/ai_icon.svg";
 import innovation_icon from "../icons/innovation_icon.svg";
 import liberty_icon from "../icons/liberty_icon.svg";
+import project_icon from "../icons/project_icon.svg";
+import article_icon from "../icons/article_icon.svg";
+import blog_icon from "../icons/blog_icon.svg";
+
 
 const items = [
   {
@@ -38,57 +42,116 @@ const About: React.FC = () => {
       </header>
 
       {/* Spotlight Kartlar */}
-      <div className="about-cards">
-        <SpotlightCard
-          className="about-spotlight-card"
-          spotlightColor="rgba(75, 218, 232, 0.75)"
-          position={{ x: 12, y: 12 }}
-        >
-          <div className="card-content">
-            <img src={ai_icon} alt="AI Icon" className="card-icon" />
-            <h3>AI Solutions</h3>
-            <p>
-              Building intelligent systems and smart automation tools to empower
-              businesses and modern workflows.
-            </p>
-          </div>
-        </SpotlightCard>
+      <div className="about-hero-containers">
+        <div className="about-cards">
+          <SpotlightCard
+            className="about-spotlight-card"
+            spotlightColor="rgba(75, 218, 232, 0.75)"
+            position={{ x: 12, y: 12 }}
+          >
+            <div className="card-content">
+              <img src={ai_icon} alt="AI Icon" className="card-icon" />
+              <h3>AI Solutions</h3>
+              <p>
+                Building intelligent systems and smart automation tools to
+                empower businesses and modern workflows.
+              </p>
+            </div>
+          </SpotlightCard>
 
-        <SpotlightCard
-          className="about-spotlight-card"
-          spotlightColor="rgba(125, 232, 75, 0.75)"
-          position={{ x: 32, y: 32 }}
-        >
-          <div className="card-content">
-            <img
-              src={innovation_icon}
-              alt="Innovation Icon"
-              className="card-icon"
-            />
-            <h3>Innovation</h3>
-            <p>
-              Creating forward-thinking solutions, experimenting with new
-              technologies, and pushing boundaries of software development.
-            </p>
-          </div>
-        </SpotlightCard>
+          <SpotlightCard
+            className="about-spotlight-card"
+            spotlightColor="rgba(125, 232, 75, 0.75)"
+            position={{ x: 32, y: 32 }}
+          >
+            <div className="card-content">
+              <img
+                src={innovation_icon}
+                alt="Innovation Icon"
+                className="card-icon"
+              />
+              <h3>Innovation</h3>
+              <p>
+                Creating forward-thinking solutions, experimenting with new
+                technologies, and pushing boundaries of software development.
+              </p>
+            </div>
+          </SpotlightCard>
 
-        <SpotlightCard
-          className="about-spotlight-card"
-          spotlightColor="rgba(232, 75, 127, 0.75)"
-          position={{ x: 24, y: 24 }}
-        >
-          <div className="card-content">
-            <img src={liberty_icon} alt="Liberty Icon" className="card-icon" />
-            <h3>Liberty</h3>
-            <p>
-              Advocating open-source, ethical coding practices, and freedom of
-              choice in tech solutions.
-            </p>
-          </div>
-        </SpotlightCard>
+          <SpotlightCard
+            className="about-spotlight-card"
+            spotlightColor="rgba(232, 75, 127, 0.75)"
+            position={{ x: 24, y: 24 }}
+          >
+            <div className="card-content">
+              <img
+                src={liberty_icon}
+                alt="Liberty Icon"
+                className="card-icon"
+              />
+              <h3>Liberty</h3>
+              <p>
+                Advocating open-source, ethical coding practices, and freedom of
+                choice in tech solutions.
+              </p>
+            </div>
+          </SpotlightCard>
+        </div>
+        <div className="about-cards">
+          <SpotlightCard
+            className="about-spotlight-card"
+            spotlightColor="rgba(75, 218, 232, 0.75)"
+            position={{ x: 12, y: 12 }}
+          >
+            <div className="card-content">
+              <img src={article_icon} alt="Article Icon" className="card-icon" />
+              <h3>Article</h3>
+              <p>
+                Building intelligent systems and smart automation tools to
+                empower businesses and modern workflows.
+              </p>
+            </div>
+          </SpotlightCard>
+
+          <SpotlightCard
+            className="about-spotlight-card"
+            spotlightColor="rgba(125, 232, 75, 0.75)"
+            position={{ x: 32, y: 32 }}
+          >
+            <div className="card-content">
+              <img
+                src={blog_icon}
+                alt="Blog Icon"
+                className="card-icon"
+              />
+              <h3>Blog</h3>
+              <p>
+                Creating forward-thinking solutions, experimenting with new
+                technologies, and pushing boundaries of software development.
+              </p>
+            </div>
+          </SpotlightCard>
+
+          <SpotlightCard
+            className="about-spotlight-card"
+            spotlightColor="rgba(232, 75, 127, 0.75)"
+            position={{ x: 24, y: 24 }}
+          >
+            <div className="card-content">
+              <img
+                src={project_icon}
+                alt="Project Icon"
+                className="card-icon"
+              />
+              <h3>Projects</h3>
+              <p>
+                Advocating open-source, ethical coding practices, and freedom of
+                choice in tech solutions.
+              </p>
+            </div>
+          </SpotlightCard>
+        </div>
       </div>
-        
 
       {/* Footer */}
       <div style={{ position: "relative", zIndex: 1, paddingBottom: "0rem" }}>
