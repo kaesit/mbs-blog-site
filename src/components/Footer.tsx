1b4e7a3806b6989c7d96e5e6ff8e4f5dca1c6933
esadabdullahkosedag <kaesoftware@gmail.com>
2026-01-23 18:54:43 +0300

    Footer has its new structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,7 @@ const Footer: React.FC = () => {
         <div className="footer-content">
           {/* Sol kısım */}
           <div className="footer-section" id="footer-brand-section">
+            <img src="../MbsLogo.png" alt="" className="footer-brand-logo" />
             <h4 className="footer-title">MBS Liberty Tech</h4>
             <p className="footer-text">
               Building innovative solutions with freedom, order, and creativity.
@@ -15,7 +16,7 @@ const Footer: React.FC = () => {
           </div>
 
           {/* Orta linkler */}
-          <div className="footer-section">
+          <div className="footer-section" id="footer-link-section">
             <h4 className="footer-title">Quick Links</h4>
             <ul className="footer-links">
               <li>
@@ -31,7 +32,7 @@ const Footer: React.FC = () => {
           </div>
 
           {/* Sosyal medya */}
-          <div className="footer-section">
+          <div className="footer-section" id="footer-social-section">
             <h4 className="footer-title">Connect</h4>
             <div className="footer-socials">
               <ul className="footer-links">
