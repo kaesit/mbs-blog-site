d50038635eaef22e8072228b22ee0de517ce117a
esadabdullahkosedag <kaesoftware@gmail.com>
2026-02-01 22:06:11 +0300

    chore: change bg button

--- src/pages/Contributing.tsx
+++ src/pages/Contributing.tsx
@@ -19,19 +19,22 @@ const ContributingPage: React.FC = () => {
         
         <div className="bg-[#070F2B] min-h-screen text-gray-200 font-sans">
             <style>
-@import url('https://fonts.googleapis.com/css2?family=Asimovian&family=Genos:ital,wght@0,300;1,300&display=swap');
+                @import url('https://fonts.googleapis.com/css2?family=Asimovian&family=Genos:ital,wght@0,300;1,300&display=swap');
             </style>
             <main className="pt-20 container mx-auto p-7">
                 <h1 className="text-4xl md:text-5xl font-[Asimovian] font-black text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-purple-600 mb-8 mt-4 uppercase tracking-wider">
                     Guideline to Contribute
                 </h1>
-                <div className="border-4 border-solid border-cyan-900 border-l-cyan-500 p-20 rounded-xl">
+                <div className="border-4 border-solid border-cyan-900 border-l-cyan-500 p-20 p-b-10 rounded-xl">
                     <article className="prose prose-invert prose-lg max-w-none prose-headings:font-[Genos] prose-text:font-[Genos] prose-headings:uppercase prose-a:text-cyan-400">
                         <ReactMarkdown>
                             {markdownContent}
                         </ReactMarkdown>
                     </article>
                 </div>
+                <button className="min-w-48 mt-6 h-12 bg-sky-500 border-solid border-sky-500 border-2 rounded-xl  transition duration-700 ease-in-out hover:bg-neutral-900">
+                    Download File
+                </button>
             </main>
         </div>
     );
